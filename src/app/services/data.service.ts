21c6f2febea5922fd1812823090c107d9d628b20
blackratio <blackratio@gmail.com>
2017-07-18 21:28:06 +0200

    code inline

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
 
    getPosts() {
       return this.http.get('https://jsonplaceholder.typicode.com/posts')
-         .map(res => res.json());
+      .map(res => res.json());
    }
 
 }
