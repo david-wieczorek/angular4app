faa7275a8296ce78dcdfa315ba3296901d936917
blackratio <blackratio@gmail.com>
2017-07-18 18:22:47 +0200

    unit test & e2e testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('a4app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to my first Angular 4 application !');
   });
 });
